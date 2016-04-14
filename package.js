5128b5fece66a831d7d12b68e204935115265192
Matteo De Micheli <mat.demicheli@gmail.com>
2016-04-14 13:21:52 +0200

    Add git url to package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'arkham:stars-rating-ui',
   version: '0.1.0',
   summary: 'User based stars rating',
-  git: '',
+  git: 'https://github.com/ARKHAM-Enterprises/meteor-stars-rating-ui',
   documentation: 'README.md'
 });
 
