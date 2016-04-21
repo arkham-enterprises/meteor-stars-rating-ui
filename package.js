3307cd2dc4e5c4b610e6a2db741fd9375764e82b
Matteo De Micheli <mat.demicheli@gmail.com>
2016-04-21 11:20:37 +0200

    version bump 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arkham:stars-rating-ui',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'User based stars rating',
   git: 'https://github.com/ARKHAM-Enterprises/meteor-stars-rating-ui',
   documentation: 'README.md'
